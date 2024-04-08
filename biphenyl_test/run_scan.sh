d59d817817a03bc3feb534c93c5d72db5ceb1975
felippe <colombarifm@hotmail.com>
2024-04-08 14:01:05 -0300

    update biphenyl results

--- biphenyl_test/run_scan.sh
+++ biphenyl_test/run_scan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf "%4s\t%12s\t%12s\n" "#deg" "#OPD_index" "#scaled_OPD_index" > opd_scan.log
+printf "%4s\t%12s\n" "#deg" "scaled_OPD_index" > opd_scan.log
 
 for n in $( seq 1 1 37 )
 do 
@@ -11,9 +11,8 @@ do
 
   deg=$( echo "(${n}-1)*5" | bc -l )
 
-  osipov_index=$( grep "Osipov index" results.log | head -1 | awk '{printf $5}' )
-  scaled_osipov_index=$( grep "Scaled Osipov index" results.log | awk '{printf $6}' )
+  scaled_osipov_index=$( grep "G0s" results.log | awk '{printf $6}' )
 
-  printf "%4i\t%12.6f\t%12.6f\n" ${deg} ${osipov_index} ${scaled_osipov_index} >> opd_scan.log
+  printf "%4i\t%12.6f\n" ${deg} ${scaled_osipov_index} >> opd_scan.log
 
 done
